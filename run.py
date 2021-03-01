6bd25c0142c65afb9e6b65ee294ef711306a7b18
Gonzalo Lardies <gon.lardies.guillen@gmail.com>
2021-03-01 12:04:17 +0100

    get annotations

--- run.py
+++ run.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
 
 import logging
 import sys
@@ -8,19 +12,43 @@ from argparse import ArgumentParser
 from cytomine import Cytomine
 from cytomine.models import AnnotationCollection
 
+__version__ = "1.0.0"
+
+def checking(params):
+
+    check = """
+    -------------------- Versión de software v{} --------------------
+
+    Parámetros introducidos:
+        - host: {}
+        - public_key: PUBLIC_KEY
+        - private_key: PRIVATE_KEY
+        - id_project: {}
+        - id_software: {}
+        - id_imagen: {}
+    """.format(__version__, params.host, params.id_project, params.id_software, params.id_image)
+
+    return print(check)
+
 
 def run(params):
-    
-    # acceso a la instancia de cytomine
-    with Cytomine(host=params.host, public_key=params.public_key, private_key=params.private_key, verbose=logging.INFO) as cytomine:
-        
-        # se define AnnotationCollection --> contiene todas las anotaciones para (id_proyecto) + (id_software)
-        annotations = AnnotationCollection(id_project=params.id_project, id_software=params.id_software, 
-                                           showWKT=True, showMeta=True, showGIS=True, showTerm=True)
-        annotations.fetch()
-        print(annotations)
 
-        # Se itera para imprimir cada anotacion 
+    with Cytomine(host=params.host, public_key=params.public_key, private_key=params.private_key, verbose=logging.INFO) as cytomine:
+
+        print(cytomine.current_user)
+
+        annotations = AnnotationCollection()
+        annotations.project = params.id_project
+        annotations.software = params.id_software
+        annotations.image = params.id_image
+        annotations.showWKT = True
+        annotations.showMeta = True
+        annotations.showGIS = True
+        annotations.showTerm = True
+        annotations.fetch()
+
+        
+        print(annotations)
         for annotation in annotations:
             print("ID: {} | Image: {} | Project: {} | Term: {} | User: {} | Area: {} | Perimeter: {} | WKT: {}".format(
                 annotation.id,
@@ -33,21 +61,10 @@ def run(params):
                 annotation.location
             ))
 
-        """
-        DUDA: la idea es: (?)
-            - pasarle al script como argumento el id de la anotacion y sacar stats de esa anotación
-            - hacerlo para todas las anotaciones dado un proyecto y un algoritmo
-
-        """
-
-
 
 if __name__ == '__main__':
 
-    # parser para seleccionar los parametros
     parser = ArgumentParser(prog="Show Region Stats")
-
-    # Parametros
     parser.add_argument('--cytomine_host', dest='host', default='viewer2.cells-ia.com', 
                         help="The Cytomine host")
     parser.add_argument('--cytomine_public_key', dest='public_key',
@@ -58,7 +75,10 @@ if __name__ == '__main__':
                         help="The project from which we want the annotations")
     parser.add_argument('--cytomine_id_software', dest='id_software',
                         help="The software wich we want the annotations")
+    parser.add_argument('--cytomine_id_image', dest="id_image",
+                        help="The image wich we want the anotations")
     params, other = parser.parse_known_args(sys.argv[1:])
     
+    checking(params)
     run(params)
     
