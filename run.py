634fac9b3076af0ed91868ea5fc70e284665f914
Gonzalo Lardies <gon.lardies.guillen@gmail.com>
2021-03-22 12:53:30 +0100

    Update run.py

--- run.py
+++ run.py
@@ -16,6 +16,7 @@ def get_stats_annotations(params):
     annotations.project = params.cytomine_id_project
     annotations.term = params.terms_to_analyze
 
+    print(params.terms_to_analyze)
     print(type(params.terms_to_analyze))
 
     if params.images_to_analyze != None:
