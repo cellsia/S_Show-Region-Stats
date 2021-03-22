db248621fcddd3da6fcb662384598aecccff5e27
Gonzalo Lardies <gon.lardies.guillen@gmail.com>
2021-03-22 12:49:38 +0100

    Update run.py

--- run.py
+++ run.py
@@ -14,10 +14,7 @@ def get_stats_annotations(params):
     annotations = AnnotationCollection()
 
     annotations.project = params.cytomine_id_project
-
-    if params.terms_to_analyze != None:
-        print("Savage!")
-        annotations.term = params.terms_to_analyze
+    annotations.term = params.terms_to_analyze
 
     if params.images_to_analyze != None:
         annotation.image = params.images_to_analyze
