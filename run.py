cad9663e0f6e87da706be9bc5fea01aa990b1dd6
Gonzalo Lardies <gon.lardies.guillen@gmail.com>
2021-03-22 11:08:17 +0100

    Update run.py

--- run.py
+++ run.py
@@ -203,7 +203,7 @@ def _load_multi_class_points(job: Job, image_id: str,  terms: list, detections: 
     annotations = AnnotationCollection()
     for idx, points in enumerate(detections.values()):
 
-        print(len(points))
+        print(points)
         multipoint = _generate_multipoints(points)
         annotations.append(Annotation(location=multipoint.wkt, id_image=image_id, id_terms=[terms[idx]]))
 
