260ba568b74b518e7a6658ab860ab2c04de30d03
Gonzalo Lardies <gon.lardies.guillen@gmail.com>
2021-03-22 14:33:03 +0100

    Update run.py

--- run.py
+++ run.py
@@ -272,14 +272,21 @@ def run(cyto_job, parameters):
             job_data = JobData(job.id, "detections", "inside_points_{}.json".format(item[0])).save()
             job_data.upload(output_path2)
 
-            with open(output_path2) as json_file:
-                detections = json.load(json_file)
-                annotation = Annotation().fetch(id=int(item[0]))
-                image = annotation.image
-                id = annotation.id
-                terms = item[2].rstrip(']').lstrip('[').split(',')
+            boolean = True
 
-                _load_multi_class_points(job, image, terms, detections)
+            for key, value in item[1].items():
+                if len(value) == 0:
+                    boolean = False
+        
+            if boolean:
+                with open(output_path2) as json_file:
+                    detections = json.load(json_file)
+                    annotation = Annotation().fetch(id=int(item[0]))
+                    image = annotation.image
+                    id = annotation.id
+                    terms = item[2].rstrip(']').lstrip('[').split(',')
+
+                    _load_multi_class_points(job, image, terms, detections)
 
             
 
