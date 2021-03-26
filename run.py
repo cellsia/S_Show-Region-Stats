792bcff3202afa90853cba221c18740568391441
Gonzalo Lardies <gon.lardies.guillen@gmail.com>
2021-03-26 02:24:33 +0100

    Update run.py

--- run.py
+++ run.py
@@ -242,11 +242,11 @@ def _load_multi_class_points(job: Job, image_id: str, detections: dict, id_: int
         
         annotations.append(Annotation(location=multipoint.wkt, id_image=image_id, id_terms=[t1[0]]))"""
         
-        """annotation = Annotation(location=multipoint.wkt, id_image=image_id, id_project=params.cytomine_id_project).save()
-        #term = Term().fetch(id=terms[idx])
-        Property(annotation, key="ID:", value=id_).save()
-        Property(annotation, key="Term:", value=term_names[idx]).save()
-        AnnotationTerm(id_anotation=id_, id_term=term_names[idx]).save()"""
+    """annotation = Annotation(location=multipoint.wkt, id_image=image_id, id_project=params.cytomine_id_project).save()
+    #term = Term().fetch(id=terms[idx])
+    Property(annotation, key="ID:", value=id_).save()
+    Property(annotation, key="Term:", value=term_names[idx]).save()
+    AnnotationTerm(id_anotation=id_, id_term=term_names[idx]).save()"""
 
     #annotations.save()
         
