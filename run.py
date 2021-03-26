d3e60b0d59fddb2e6f042878e403ebc5a51cd251
Gonzalo Lardies <gon.lardies.guillen@gmail.com>
2021-03-26 04:00:13 +0100

    Update run.py

--- run.py
+++ run.py
@@ -221,9 +221,7 @@ def _load_multi_class_points(job: Job, image_id: str, detections: dict, id_: int
 
         multipoint = _generate_multipoints(points)
         
-
         l = [t.name for t in termscol]
-        termscol = TermCollection().fetch_with_filter("project", params.cytomine_id_project)
         t1 = [t.id for t in termscol if t.name == term_name]
         
         if not(term_name in l):
