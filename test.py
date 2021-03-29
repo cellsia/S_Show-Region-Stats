fc4f0dba9922457bba0019bdb5b04fe25a1ba774
Gonzalo Lardies <gon.lardies.guillen@gmail.com>
2021-03-29 10:58:04 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 import logging
 import sys
+import json
+import os
 from argparse import ArgumentParser
 
 from cytomine import Cytomine
 from cytomine.models import AnnotationCollection, JobCollection, JobData
-from cytomine.models.software import JobDataCollection
+from cytomine.models.software import JobDataCollection, JobParameterCollection
 
 
 def get_stats_annotations(params):
@@ -40,26 +42,57 @@ def get_stats_annotations(params):
         return annotations
 
 def get_results(params):
+    
     with Cytomine(host=params.cytomine_host, public_key=params.cytomine_public_key, private_key=params.cytomine_private_key, verbose=logging.INFO) as cytomine:
 
+        results = [] # array con resultados 
+        equiv = {} # diccionario de equivalencias (filename - cytomine_image_term_id)
+
+        # sacamos lista con todos los job id's
         jobs = JobCollection()
         jobs.project = params.cytomine_id_project
         jobs.fetch()
+        jobs_ids = [job.id for job in jobs]
 
-        job_ids = [job.id for job in jobs]
-        jobs = None
 
-        for job_id in job_ids:
+        for job_id in jobs_ids:
 
-            jobdatacol = JobDataCollection().fetch_with_filter("job", job_id)
+            # para cada job sacamos prametros y coleccion de datos
+            jobparamscol = JobParameterCollection().fetch_with_filter(key="job", value=job_id)
+            jobdatacol = JobDataCollection().fetch_with_filter(key="job", value=job_id)
 
-            for jobdata in jobdatacol:        
+            for job in jobdatacol:
 
-                jobdata = JobData().fetch(jobdata.id)
-                print(jobdata.filename)
+                jobdata = JobData().fetch(job.id)
+                filename = jobdata.filename
 
-        
-        return None
+                allowed_params = ["cytomine_image", "cytomine_id_image", "cytomine_image_instance"]
+                [equiv.update({filename:int(param.value)}) for param in jobparamscol if (str(param.split(" : ")[1] in allowed_params)) ]
+
+                # si el .json tiene "detections en el nombre de archivo lo descargamos y lo metemos en la carpeta tmp/"
+                if "detections" in filename:
+                    try:
+                        jobdata.download(os.path.join("tmp/", filename))
+                    except AttributeError:
+                        continue
+
+        # cargamos los resultados a partir de los archivos que hemos descargado
+        temp_files = os.listdir("tmp")
+        for i in range(0, len(temp_files)):
+            if temp_files[i][-4:] == "json":
+                filename = temp_files[i]
+                try:
+                    image = equiv[filename] # recuperamos la imagen a la que hace refereancia cada archivo bassandonos en el dic de equivalencias
+                    with open("tmp/"+filename, 'r') as json_file:
+                        data = json.load(json_file)
+                        json_file.close()
+                    results.append({"image":image,"data":data}) # añadimos el resultado con su imagen asociada
+                except KeyError:
+                    continue
+
+        os.system("cd tmp&&rm detections*") # eliminamos archivos temporales
+
+        return results
 
 
 if __name__ == '__main__':
@@ -85,4 +118,8 @@ if __name__ == '__main__':
 
     #get_stats_annotations(params)
 
-    get_results(params)
+    results = get_results(params)
+    for r in results:
+        print(r, end="\n\n\n")
+    
+    
