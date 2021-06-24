7a332ed2243978b8e5d3356a88b180f65753e0a4
GonzaloLardies <gon.lardies.guillen@gmail.com>
2021-06-24 10:59:29 +0200

    v1.3.4

--- run.py
+++ run.py
@@ -21,7 +21,7 @@ from cytomine.models.property import Property, PropertyCollection
 
 
 # script version 
-__version__ = "1.3.3" 
+__version__ = "1.3.4" 
 
 
 
@@ -336,7 +336,7 @@ def run (cyto_job, parameters):
 
             output_path = os.path.join(working_path, "inside_points_{}.json".format(item[0]))
             f = open(output_path, "w+")
-            json.dump(item[1], f)
+            json.dump(item[1], f.__dict__)
             f.close()
 
             job_data = JobData(job.id, "detections", "inside_points_{}.json".format(item[1]).save())
